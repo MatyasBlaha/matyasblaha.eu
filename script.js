38483446af88d7b9aa19141be96be85c4d05c878
MatyasBlaha <matyasblaha1@gmail.com>
2020-11-30 10:30:25 +0100

    test 0.1

---
+++ script.js
@@ -0,0 +1,30 @@
+    function toggleMenu(){
+    var navigation = document.querySelector('.navigation');
+    var toggle = document.querySelector('.toggle');
+    navigation.classList.toggle('active');
+    toggle.classList.toggle('active');
+}
+
+
+/*      Více proměných
+
+var firstName = "Matyáš";
+var secondName = "Bláha";
+var age = 17;
+var married = true;
+
+console.log("Tento člověk se jmenuje " + firstName + " " + secondName + " a je mu " + age + " let a je ženatý - " + married )
+
+*/
+
+
+//alert("Ahoj, já jsem Maty");
+//confirm("Jsi plnoletý?")
+
+/* 
+
+var age = prompt("Kolike je ti let?");
+var adult = age - 18;
+console.log(adult)
+
+*/
